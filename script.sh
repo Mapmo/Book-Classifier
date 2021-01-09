5a4a6b82afa6610c06e97312a0678f2b9d0af5b3
mapmo <mapmoeu@gmail.com>
2021-01-09 23:38:52 +0200

    Create a python script for extraction of the data from books and categories

--- script.sh
+++ script.sh
@@ -1,22 +1,23 @@
+#!/bin/bash
+
 tmpfile=$(mktemp)
 tmpfile2=$(mktemp)
 vowel="аъоуеияю"
 
-while read file; do 
-	> "$tmpfile"
-	> "$tmpfile2"
+while read file; do
+    > "$tmpfile"
+    > "$tmpfile2"
 
-	egrep -oi "([а-я]{4,})" "$file" | egrep "^([^$vowel]+[$vowel]*[^$vowel]*[$vowel]*)|^([$vowel]+[^$vowel]*[$vowel]*[^$vowel]*)|^(.{5})" -io | sort -u > "$tmpfile"
+    egrep -oi "([а-я]{4,})" "$file" | egrep "^([^$vowel]+[$vowel]*[^$vowel]*[$vowel]*)|^([$vowel]+[^$vowel]*[$vowel]*[^$vowel]*)|^(.{5})" -io | sort | uniq -c | sort -rn | head -n 2500 | awk '{print $2}' > "$tmpfile"
 
-	counter=0
-	while read word; do
-		if grep -q "^$word" $(ls ~/Git/Book-Classifier/noise_data/*); then
-			continue
-		fi
-		echo "$word" | awk '{print tolower($0)}' >> "$tmpfile2"
-	done < "$tmpfile"
+    while read word; do
+        if grep -q "^$word" $(ls ~/Git/Book-Classifier/noise_data/*); then
+            continue
+        fi
+        echo "$word" | awk '{print tolower($0)}' >> "$tmpfile2"
+    done < "$tmpfile"
 
-	sort "$tmpfile2" | uniq -c | sort -rn | head -n 2000 | awk '{print $2}' >> words_per_book
+    head -n 2000 "$tmpfile2" >> words_per_book
 done < "$1"
 
 rm "$tmpfile"

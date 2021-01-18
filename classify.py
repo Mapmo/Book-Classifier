6f6e9d6e9c7c376d6b0895ee4ab8f3579f68be52
mapmo <mapmoeu@gmail.com>
2021-01-18 23:44:54 +0200

    Add a machine learning script that analyzes the data from dataset.json

---
+++ classify.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import json
+import pandas as pd
+import sys
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+# Binary Relevance
+from sklearn.multiclass import OneVsRestClassifier
+# Performance metric
+from sklearn.metrics import f1_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+books_fd = open(sys.argv[1])
+books_json = json.loads(books_fd.read())
+for id in books_json.keys():
+    books_json[id]["words"] = ' '.join(books_json[id]["words"])
+
+books = pd.DataFrame.from_dict(books_json).transpose()
+
+
+multilabel_binarizer = MultiLabelBinarizer()
+multilabel_binarizer.fit(books['genre'])
+
+y = multilabel_binarizer.transform(books['genre'])
+
+tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=10000)
+
+xtrain, xval, ytrain, yval = train_test_split(books['words'], y, test_size=0.2, random_state=9)
+
+xtrain_tfidf = tfidf_vectorizer.fit_transform(xtrain)
+xval_tfidf = tfidf_vectorizer.transform(xval)
+
+lr = LogisticRegression()
+clf = OneVsRestClassifier(lr)
+clf.fit(xtrain_tfidf, ytrain)
+
+# predict probabilities
+y_pred_prob = clf.predict_proba(xval_tfidf)
+t = 0.25  # threshold value
+y_pred_new = (y_pred_prob >= t).astype(int)
+result = f1_score(yval, y_pred_new, average="micro")
+print(result)

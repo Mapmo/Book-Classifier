8411b03da089e9eb39a29213bc89bc5810aa92a2
Martin Iliev <mapmoeu@gmail.com>
2021-02-07 22:40:36 +0200

    Imrpove the extraction script so that it no longer requires the noise data and the books to be in the same directory

--- extract.py
+++ extract.py
@@ -10,11 +10,10 @@ import string
 import sys
 
 
-def read_file(path):
-    tmp_fd = open(path)
-    tmp_str = tmp_fd.read()
-    tmp_fd.close()
-    return tmp_str
+def read_file(filename):
+    path = os.path.join(os.path.dirname(sys.argv[0]), "noise_data", filename)
+    with open(path) as tmp_fd:
+        return tmp_fd.read()
 
 
 def dict_word(word, words, val):
@@ -24,39 +23,38 @@ def dict_word(word, words, val):
         words[word] = val
 
 
-cities = read_file("noise_data/city_names.txt")
-countries = read_file("noise_data/country_names.txt")
-stops = read_file("noise_data/grammar_words.txt")
-names = read_file("noise_data/human_names.txt")
+cities = read_file("city_names.txt")
+countries = read_file("country_names.txt")
+stops = read_file("grammar_words.txt")
+names = read_file("human_names.txt")
 
 stemmer = BulStemmer.from_file('stem-context-1')
 
 
 def extract_words(book, N):
-    book_fd = open(book)
-    raw_words = dict()
-    words = dict()
-    try:
-        for line in book_fd:
-            for word in line.split():
-                dict_word(word, raw_words, 1)
-    except UnicodeDecodeError:
-        print("Book", book, "has some non utf-8 chars and was not scanned to the end")
-
-    for word, count in raw_words.items():
-        word = word.strip(string.punctuation + "„“—")
-        if len(word) < 2 or word in cities or word in names or word in stops:
-            continue
-        word = word.lower()
-        if re.search(r"[^а-я]", word):
-            continue
+    with open(book) as book_fd:
+        raw_words = dict()
+        words = dict()
         try:
-            word = stemmer.stem(word)
-            dict_word(word, words, count)
-        except KeyError:
-            continue
-    raw_words.clear()
-    book_fd.close()
+            for line in book_fd:
+                for word in line.split():
+                    dict_word(word, raw_words, 1)
+        except UnicodeDecodeError:
+            print("Book", book, "has some non utf-8 chars and was not scanned to the end")
+
+        for word, count in raw_words.items():
+            word = word.strip(string.punctuation + "„“—")
+            if len(word) < 2 or word in cities or word in names or word in stops:
+                continue
+            word = word.lower()
+            if re.search(r"[^а-я]", word):
+                continue
+            try:
+                word = stemmer.stem(word)
+                dict_word(word, words, count)
+            except KeyError:
+                continue
+        raw_words.clear()
 
     top_words = Counter(words).most_common(N)
     words.clear()
